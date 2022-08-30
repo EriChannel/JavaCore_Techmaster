81ad219fa65eecd66524673d04fdd9ed0ba3296e
Lập trình cùng Eri <erichannel96@gmail.com>
2022-08-30 10:04:46 +0700

    Class Math

--- 03_Class_Math_Constant_operation/ClassMath/src/Main.java
+++ 03_Class_Math_Constant_operation/ClassMath/src/Main.java
@@ -1,29 +1,31 @@
+import java.util.TimeZone;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("pi = " + Math.PI);
+        int r = 5;
+        double area = r*r * Math.PI;
+        System.out.println("Diện tích hình tròn: " + area);
+
         int x = 5;
         int y = 9;
+        System.out.println("Giá trị tuyệt đối của y = " + Math.abs(y));
 
-        double s = x * x * Math.PI;
-        System.out.println("Diện tích: " + s);
+        System.out.println("Số lớn nhất trong hai số x và y là : " + Math.max(x, y));
+        System.out.println("Số nhỏ nhất trong hai số x và y là : " +Math.min(x, y));
 
-        //Tính luỹ thừa
-        System.out.println(Math.pow(x, 5));
+        System.out.println("Căn bậc hai: " +Math.sqrt(y));
+        System.out.println("Lũy thừa: " + Math.pow(y, x));
 
-        //Khai căn
-        System.out.println(Math.sqrt(y));
+        System.out.println("Làm trong tăng: " +Math.ceil(area));
+        System.out.println("Làm tròn giảm: " + Math.floor(area));
 
-        System.out.println("Số lớn nhất trong hai số : " + Math.max(x, y));
-        System.out.println("Số nhỏ nhất trong hai số: " + Math.min(x, y));
+        System.out.println("Sin " + Math.sin(Math.PI/2));
+        System.out.println("Cos " + Math.cos(Math.PI));
 
-        System.out.println("Giá trị tuyệt đối: " + Math.abs(y));
+        System.out.println("Đổi từ radian sang độ: " +Math.toDegrees(Math.PI/2));
+        System.out.println("Đổi từ độ sang radian: " + Math.toRadians(45));
 
-        double z = 5.6;
-        System.out.println("Làm tròn tăng: " + Math.ceil(z));
-        System.out.println("Làm tròn giảm: " + Math.floor(z));
-
-        System.out.println("Số ngẫu nhiên: " + Math.random());
-
-        System.out.println("Tính sin: " + Math.sin(z));
-        System.out.println("Tính cos: " + Math.cos(z));
+        System.out.println("Sinh số ngẫu nhiên từ 0 đến 1: " + Math.random());
     }
 }
