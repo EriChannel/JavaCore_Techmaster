ba9bfa769294f104896d950c1d4659e5cc4f5a92
Lập trình cùng Eri <erichannel96@gmail.com>
2022-09-04 11:09:58 +0700

    maven

---
+++ 17_File_Handling/first_app/src/main/java/org/example/Person.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Person {
+    private String name;
+    private int age;
+    private String address;
+
+
+}
